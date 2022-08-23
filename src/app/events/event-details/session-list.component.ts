7bc17ee13fa9d14bcb14d06f57b42602f3df7be4
likhilsudheer <109787868+likhilsudheer@users.noreply.github.com>
2022-08-23 17:23:44 +0530

    Delete src directory

--- src/app/events/event-details/session-list.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, Input } from "@angular/core";
-@Component({
-    selector:'session-list',
-    templateUrl:'./session-list.component.html'
-})
-export class SessionListComponent{
-@Input() sessions:any
-}
